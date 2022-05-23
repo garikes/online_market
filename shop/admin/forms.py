49cfc427bb20ba10eba509b88e1d9de169f0c7d9
Marian <petruskomaran@gmail.com>
2022-05-23 20:38:15 +0300

    First

---
+++ shop/admin/forms.py
@@ -0,0 +1,18 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+
+class RegistrationForm(Form):
+    first_name = StringField('Firstname', [validators.Length(min=4, max=25)])
+    last_name = StringField('Lastname', [validators.Length(min=4, max=25)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+
+class LoginForm(Form):
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
+    password = PasswordField('New Password', [validators.DataRequired()])
---
+++ shop/customers/forms.py
@@ -0,0 +1,35 @@
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, validators, Form, ValidationError
+from flask_wtf.file import FileRequired, FileAllowed, FileStorage, FileField
+from flask_wtf import FlaskForm
+from .model import Register
+
+
+class CustomerRegisterForm(FlaskForm):
+    first_name = StringField('First name: ')
+    last_name = StringField('Last name: ')
+    username = StringField('Username: ', [validators.DataRequired()])
+    email = StringField('Email: ', [validators.Email(), validators.DataRequired()])
+    password = PasswordField('Password: ', [validators.DataRequired(),
+                                            validators.EqualTo('confirm', message=' Both password must match! ')])
+    confirm = PasswordField('Repeat Password: ', [validators.DataRequired()])
+
+    country = StringField('Country: ', [validators.DataRequired()])
+    city = StringField('City: ', [validators.DataRequired()])
+    contact = StringField('Contact: ', [validators.DataRequired()])
+    address = StringField('Address: ', [validators.DataRequired()])
+
+    profile = FileField('Profile', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'])])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        if Register.query.filter_by(username=username.data).first():
+            raise ValidationError("This username is already in use!")
+
+    def validate_email(self, email):
+        if Register.query.filter_by(email=email.data).first():
+            raise ValidationError("This email is already in use!")
+
+
+class CustomerLoginForm(FlaskForm):
+    email = StringField('Email: ', [validators.Email(), validators.DataRequired()])
+    password = PasswordField('Password: ', [validators.DataRequired()])
